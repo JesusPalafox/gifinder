a5d68577157f9c1625652832a93c0a8c8314f250
JesusPalafox <jpalafooxlopez@gmail.com>
2026-09-11 00:17:32 -0700

    chore: crear proyecto base con Vite y TypeScript

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './styles/style.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<main>
+<h1>GIFinder</h1>
+<p>Explora y descubre contenido multimedia</p>
+</main>
+`;  
+
